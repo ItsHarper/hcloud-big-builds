1167e2a1e814b69699edee5c15dcbd5a28ff86c7
ItsHarper <git-f4cbe@harperandrews.org>
2025-09-20 17:48:24 -0500

    Download and run nushell

--- init.sh
+++ init.sh
@@ -4,12 +4,25 @@ set -euo pipefail
 # Intended to be run on a Debian 12 (bookworm) Compute Engine VM
 
 NU_VERSION="0.107.0"
+NU_ARCHIVES_DIR="$HOME/nushell-bin-archives"
+NU_ARCHIVE_FILENAME="nu-$NU_VERSION-x86_64-unknown-linux-gnu.tar.gz"
+NU_ARCHIVE_PATH="$NU_ARCHIVES_DIR/$NU_ARCHIVE_FILENAME"
+NU_BIN_DIR="$HOME/nushell-bin"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Verify that we're running in Google Cloud, so we don't have to worry
 # too much about accidentally fucking up someone's everyday setup
 # TODO(Harper): Look for "Metadata-Flavor: Google"
 curl metadata.google.internal -i
 
-wget "https://github.com/nushell/nushell/releases/download/$NU_VERSION/nu-$NU_VERSION-x86_64-unknown-linux-gnu.tar.gz"
+# Download nushell
+mkdir -p "$NU_ARCHIVES_DIR"
+echo https://github.com/nushell/nushell/releases/download/"$NU_VERSION"/"$NU_ARCHIVE_FILENAME"
+curl --location https://github.com/nushell/nushell/releases/download/"$NU_VERSION"/"$NU_ARCHIVE_FILENAME" > "$NU_ARCHIVE_PATH"
 
-echo "init.sh complete"
+# Extract nushell
+mkdir -p "$NU_BIN_DIR"
+tar xvzf "$NU_ARCHIVE_PATH" --strip-components=1 --directory "$NU_BIN_DIR"
+
+echo "Continuing with nushell"
+"$NU_BIN_DIR/nu" "$SCRIPT_DIR"/run.nu
