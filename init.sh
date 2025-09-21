d337db4dbc48b740f6412fa4859d76cf157def82
ItsHarper <git-f4cbe@harperandrews.org>
2025-09-21 12:47:19 -0500

    Skip nushell install if version matches

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ NU_ARCHIVES_DIR="$HOME/nushell-bin-archives"
 NU_ARCHIVE_FILENAME="nu-$NU_VERSION-x86_64-unknown-linux-gnu.tar.gz"
 NU_ARCHIVE_PATH="$NU_ARCHIVES_DIR/$NU_ARCHIVE_FILENAME"
 NU_BIN_DIR="$HOME/nushell-bin"
+NU_PATH="$NU_BIN_DIR/nu"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Verify that we're running in Google Cloud, so we don't have to worry
@@ -15,14 +16,16 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # TODO(Harper): Look for "Metadata-Flavor: Google"
 curl metadata.google.internal -i
 
-# Download nushell
-mkdir -p "$NU_ARCHIVES_DIR"
-echo https://github.com/nushell/nushell/releases/download/"$NU_VERSION"/"$NU_ARCHIVE_FILENAME"
-curl --location https://github.com/nushell/nushell/releases/download/"$NU_VERSION"/"$NU_ARCHIVE_FILENAME" > "$NU_ARCHIVE_PATH"
+# Download nushell if necessary
+if [[ ! -f $NU_PATH ]] || [[ "$($NU_PATH --version)" != "$NU_VERSION" ]]; then
+	echo "Downloading nushell"
+	mkdir -p "$NU_ARCHIVES_DIR"
+	curl --location https://github.com/nushell/nushell/releases/download/"$NU_VERSION"/"$NU_ARCHIVE_FILENAME" > "$NU_ARCHIVE_PATH"
 
-# Extract nushell
-mkdir -p "$NU_BIN_DIR"
-tar xvzf "$NU_ARCHIVE_PATH" --strip-components=1 --directory "$NU_BIN_DIR"
+	echo "Extracting nushell"
+	mkdir -p "$NU_BIN_DIR"
+	tar xvzf "$NU_ARCHIVE_PATH" --strip-components=1 --directory "$NU_BIN_DIR"
+fi
 
-echo "Continuing with nushell"
+echo "Running the main script using nushell"
 "$NU_BIN_DIR/nu" "$SCRIPT_DIR"/run.nu
