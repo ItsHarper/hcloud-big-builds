52f1d84b514dbbd13319a9446fb2747d189ac85e
ItsHarper <git-f4cbe@harperandrews.org>
2025-10-01 15:01:56 -0500

    Fix `lunch` invocations

--- vm-scripts/presets/graphene-os/run-build-graphene/generate-pixel-vendor-files.sh
+++ vm-scripts/presets/graphene-os/run-build-graphene/generate-pixel-vendor-files.sh
@@ -3,5 +3,5 @@ set -eo pipefail
 
 cd /mnt/build-root
 source build/envsetup.sh
-lunch "$BUILD_TARGET-$BUILD_VARIANT"
+lunch "$BUILD_TARGET-cur-$BUILD_VARIANT"
 adevtool generate-all -d $BUILD_TARGET
